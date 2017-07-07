f2176b383b0a62d2c25ff6a0af768c7dfa3a8527
Yulei <yulei@zhixindu.com>
2017-07-07 17:32:19 +0800

    删除我的代码

--- src/test/java/CodeGenerator.java
+++ src/test/java/CodeGenerator.java
@@ -16,9 +16,9 @@ import static com.company.project.core.ProjectConstant.*;
  */
 public class CodeGenerator {
     //JDBC配置，请修改为你项目的实际配置
-    private static final String JDBC_URL = "jdbc:mysql://99.48.18.214:3306/test";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test";
     private static final String JDBC_USERNAME = "root";
-    private static final String JDBC_PASSWORD = "1qaz@WSX";
+    private static final String JDBC_PASSWORD = "123456";
     private static final String JDBC_DIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
 
     private static final String PROJECT_PATH = System.getProperty("user.dir");//项目在硬盘上的基础路径
